53f30419cd9fdcc6053e4fab1eec2eb6092911fc
Robert <robethuku11@gmail.com>
2023-01-22 13:33:08 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({articles}) {
+  console.log(articles)
+const renderArticles = articles.map(article => {
+  return <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>
+})
+ 
+return <main>
+       {renderArticles}
+
+      </main>
+}
+
+
+
+
+
+
+
+
+export default  ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+
+function Header({name}) {
+
+  return <h1>{name}</h1>
+
+}
+
+
+
+export default Header;
